7ceb8c05c04bfc2b026ace5970d823b0ee443717
Amip3 <113936859+Amip3@users.noreply.github.com>
2022-09-19 16:13:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My New Healthy Menu for this Month')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Apple & Blueberry Oatmal')
+streamlit.text('Berry smothie and yogurt Smoothie')
+streamlit.text('Avocado toast with organic Egg')
