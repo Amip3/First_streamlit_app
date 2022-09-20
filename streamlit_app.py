ebc9c577b93944ebc170c1499329db93d29f88a1
Amip3 <113936859+Amip3@users.noreply.github.com>
2022-09-20 11:51:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ streamlit.text(' Avocado toast with organic Egg')
 
 import pandas
 
-streamlit.title(' :banana::strawberry: Build Your Own Fruit Smoothie 🍋 :lemon:')
+streamlit.title(' :banana::strawberry: Build Your Own Fruit Smoothie 🍋 🍍')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
